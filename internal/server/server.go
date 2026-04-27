8adc746beb7dc60538702fe22894182ece5267d7
eliott <eliott.thomann@gmail.com>
2026-04-27 18:41:21 +0200

    fix: parameter types in New

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,7 +1,7 @@
 // Package server - Gère le serveur HTTP et la connexion WebSocket avec le navigateur
 //
 // Infos :
-// * Pipeline de communication avec les autres processus : 
+// * Pipeline de communication avec les autres processus :
 //   * Navigateur --WS--> server --stdout--> application
 //   * application --stdin--> server --WS--> Navigateur
 
@@ -28,7 +28,7 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
-func New(addr, port, log *logger.Logger) *Server {
+func New(addr string, port string, log *logger.Logger) *Server {
 	return &Server{
 		addr: addr,
 		port: port,
@@ -48,7 +48,7 @@ func (s *Server) Run() error {
 	return http.ListenAndServe(addr, mux)
 }
 
-// handleWS - handler HTTP qui upgrade la connexion en WebSocket, 
+// handleWS - handler HTTP qui upgrade la connexion en WebSocket,
 // puis lit en boucle les messages du navigateur et les écrit sur stdout
 func (s *Server) handleWS(w http.ResponseWriter, r *http.Request) {
 	cnx, err := upgrader.Upgrade(w, r, nil)

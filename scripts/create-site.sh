47d0ac9432c2bc6f3b994f23ed6e2486a6ce612c
Elias <elias.lorgnier@mail.com>
2026-06-14 17:47:43 +0200

    create site now working

--- scripts/create-site.sh
+++ scripts/create-site.sh
@@ -3,27 +3,19 @@
 i=$1
 n=$2
 
-rm -f /tmp/in_app$1 /tmp/out_app$1 /tmp/in_ctl$1 /tmp/out_ctl$1 /tmp/in_net$1 /tmp/out_net$1
+rm /tmp/in_net$i /tmp/out_net$i
 
 
 
-
-echo "Creating Fifo"
-mkfifo /tmp/in_app$i /tmp/out_app$i /tmp/in_ctl$i /tmp/out_ctl$i /tmp/in_net$i /tmp/out_net$i
+mkfifo /tmp/in_net$i /tmp/out_net$i
 
 
 
-#tee /tmp/in_net$i & PID1=$!
-PID1=2334
-cat /tmp/out_net$i & PID2=$!
-
-echo "Cat pid is" $PID2
-
-#exec <> /tmp/in_net$i
-#exec <> /tmp/out_net$i
-
-./bin/net -n "net$i" -id $i -ttout $PID2 \
-    < /tmp/in_net$i > /tmp/out_net$i &
-
+echo "Starting cat"
+cat /tmp/out_net$i & PID1=$!
 sleep 1
-tee /tmp/in_net$i 
+echo "Starting net"
+./bin/net -n net$i -id $i -next $n -ttout $PID1 \
+        < /tmp/in_net$i > /tmp/out_net$i &
+sleep 1
+tee /tmp/in_net$i
